edc513e17e8f0b003909d1f3fa2657f91effb3c1
Vilen71 <119299151+Vilen71@users.noreply.github.com>
2023-01-22 23:55:23 +0400

    Update modules.js

--- js/modules.js
+++ js/modules.js
@@ -1,17 +1,2 @@
-import svetofor from "./modules/svetofor.js"
-import timer from "./modules/timer.js"
-import outNumber from "./modules/numberRun.js"
-import secundomer from "./modules/secundomer.js"
-import calculator from "./modules/calc.js"
-import invest from "./modules/invest.js"
 import blackjack from "./modules/blackjack.js"
-
-svetofor()
-timer(".data", "12 mar 2023 13:00:00")
-// outNumber()
-secundomer()
-invest()
-calculator()
 blackjack()
-
-// export { svetofor, timer, outNumber, secundomer, calculator }
